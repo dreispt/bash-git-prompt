40b3473aaafee4aa014dc8b8181f81b879884bd7
Daniel Reis <dreis.pt@gmail.com>
2014-07-27 16:01:17 +0100

    Customize

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -6,7 +6,7 @@ GIT_PROMPT_PREFIX="["                 # start of the git info string
 GIT_PROMPT_SUFFIX="]"                 # the end of the git info string
 GIT_PROMPT_SEPARATOR="|"              # separates each item
 
-GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
+GIT_PROMPT_BRANCH="${Green}"        # the git branch that is active in the current directory
 GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
 GIT_PROMPT_CONFLICTS="${Red}✖"        # the number of files in conflict
 GIT_PROMPT_CHANGED="${Blue}✚"         # the number of changed files
@@ -19,10 +19,10 @@ GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" re
 GIT_PROMPT_COMMAND_OK="${Green}✔ "    # indicator if the last command returned with an exit code of 0
 GIT_PROMPT_COMMAND_FAIL="${Red}✘ "   # indicator if the last command returned with an exit code of other than 0
 
-GIT_PROMPT_START_USER="${Yellow}${PathShort}${ResetColor}"
-GIT_PROMPT_START_ROOT="${Yellow}${PathShort}${ResetColor}"
-GIT_PROMPT_END_USER=" \n${White}${Time12a}${ResetColor} $ "
-GIT_PROMPT_END_ROOT=" \n${White}${Time12a}${ResetColor} # "
+GIT_PROMPT_START_USER="${Blue}${PathShort}${ResetColor}"
+GIT_PROMPT_START_ROOT="${Blue}${PathShort}${ResetColor}"
+GIT_PROMPT_END_USER=" $ "
+GIT_PROMPT_END_ROOT=" # "
 
 # Please do not add colors to these symbols
 GIT_PROMPT_SYMBOLS_AHEAD="↑·"         # The symbol for "n versions ahead of origin"
